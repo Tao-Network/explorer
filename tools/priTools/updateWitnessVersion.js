e1c93fdd0c6bd46c6b01806f7d97b5f2208e2174
homeMac <724104612@qq.com>
2019-03-27 11:55:12 +0800

    tool

--- tools/priTools/updateWitnessVersion.js
+++ tools/priTools/updateWitnessVersion.js
@@ -3,13 +3,23 @@ var mongoose = require('mongoose');
 var Witness = mongoose.model('Witness');
 var Block = mongoose.model('Block');
 
+var hex2ascii = function (hexIn) {
+    var hex = hexIn.toString();
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
 async function test(){
   let witnesses = await Witness.aggregate([{$lookup:{from:"blocks",localField:"lastCountTo",foreignField:"number",as: "block"}}]).exec()
   for (var i = 0; i < witnesses.length; i++) {
-    let extraData = witnesses[i].block[0].extraData;
-    let version = extraData.slice(6,8)+"."+extraData.slice(8,10)+"."+extraData.slice(10,12);
-    // console.log(witnesses[i].witness, version);
-    if (witnesses[i].witness && witnesses[i].witness.length>0) {
+    let extraData = hex2ascii(witnesses[i].block[0].extraData);
+    if (extraData && extraData.length > 5) {
+      // console.log(extraData);
+      let version = extraData.charCodeAt(3)+"."+extraData.charCodeAt(4)+"."+extraData.charCodeAt(5);
+      // console.log(witnesses[i].witness, version);
+      // console.log(witnesses[i].witness, version);
       await Witness.update({"witness":witnesses[i].witness},
         {$set:{"version": version}}
       ).exec()
