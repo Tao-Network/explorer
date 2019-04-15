86d623a27c799cfecd82ccda441e898480df700b
homeMac <724104612@qq.com>
2019-04-15 10:48:52 +0800

    f

--- tools/priTools/collect20RichestAddr.js
+++ tools/priTools/collect20RichestAddr.js
@@ -10,8 +10,8 @@ async function test(){
   let addresses = await Address.find({}).limit(20).sort({balance:-1}).exec();
   for (var i = 0; i < addresses.length; i++) {
     let balance = web3.eth.getBalance(addresses[i].addr);
-    balance = web3.fromWei(balance, "ether");
-    console.log(addresses[i].addr, balance);
+    balance = web3.fromWei(balance, "ether").toString();
+    console.log(addresses[i].addr, balance, addresses[i].balance);
   }
 }
 
