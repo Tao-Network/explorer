cde59f128cbeb423e55b0f750fa82d9ea2ef51ab
homeMac <724104612@qq.com>
2019-04-15 10:44:43 +0800

    f

--- tools/priTools/collect20RichestAddr.js
+++ tools/priTools/collect20RichestAddr.js
@@ -1,7 +1,10 @@
 require( '../../db.js' );
 var mongoose = require('mongoose');
 var Address = mongoose.model('Address');
-var web3 = require('./../../routes/web3relay').web3;
+// var web3 = require('./../../routes/web3relay').web3;
+var Web3 = require('web3');
+var rpc = "http://localhost:9646";
+var web3 = new Web3(new Web3.providers.HttpProvider(rpc));
 
 async function test(){
   let addresses = await Address.find({}).limit(20).sort({balance:-1}).exec();
