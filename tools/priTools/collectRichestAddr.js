2daca84d19f21d3c2a5fb07c6f746e9c99bd000b
homeMac <724104612@qq.com>
2019-06-27 11:52:31 +0800

    tool opt

--- tools/priTools/collectRichestAddr.js
+++ tools/priTools/collectRichestAddr.js
@@ -6,20 +6,23 @@ var Web3 = require('web3');
 var rpc = "http://localhost:9646";
 var web3 = new Web3(new Web3.providers.HttpProvider(rpc));
 
-async function test(){
-  let addresses = await Address.find({}).limit(1000).sort({balance:-1}).exec();
-  let result = new Array(21);
+async function test(num){
+  num = Number(num);
+  let addresses = await Address.find({}).limit(num * 30).sort({balance:-1}).exec();
+  let result = new Array(num+1);
   result.fill({addr:"",balance:0});
   for (let i = 0; i < addresses.length; i++) {
     let balance = web3.eth.getBalance(addresses[i].addr);
     balance = web3.fromWei(balance, "ether");
     let balancenum = Number(balance)
-    result[20] = {addr:addresses[i].addr,balance:balancenum};
+    result[num] = {addr:addresses[i].addr,balance:balancenum};
     for (let i = result.length - 1; i > 0; i--) {
       if (result[i].balance > result[i-1].balance) {
         let temp = result[i];
         result[i] = result[i-1];
         result[i-1] = temp
+      } else {
+        break;
       }
     }
     // console.log(addresses[i].addr, balancenum);
@@ -27,4 +30,4 @@ async function test(){
   console.log(result);
 }
 
-test()
+test(process.argv[2])
